e96e44dd1c65f3b8e6436b008209597897fe0cb4
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2019-12-19 12:41:56 -0600

    Se agregaron variables, para la conexion de la base de datos de manera local y la conexion con mongo atlas

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+/*
+ENTORNO
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*
+Base de datos
+*/
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'; //Conexión local
+} else {
+    urlDB = 'mongodb+srv://Victoriasaurio:f98ups0SRWjoKTA1@cluster0-wdeni.mongodb.net/cafe'; /*Se utilizó una variable de entorno para MONGO*/
+}
+process.env.URLDB = urlDB; //Variable para la conexión a la base de datos
