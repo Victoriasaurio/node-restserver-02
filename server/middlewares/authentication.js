339dafad1a60ef75b9656f2348f46f11b5e121f4
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2019-12-28 14:37:34 -0600

    Se añadió un token para autenticar las imágenes y los usuarios pueden visialuzarlas

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -41,7 +41,30 @@ let verificaAdmin_Role = (req, res, next) => {
     }
 };
 
+/*
+Verifica TOKEN para IMAGEN
+*/
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario; //decoded = payload
+        next();
+    });
+}
+
 module.exports = {
     VerificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
